62eecd119c21076c6d93d66a0f155bc2487d667b
Ngô Đạt <supodatngo123@gmail.com>
2021-01-03 08:13:21 +0700

    Duyet do thi theo chieu sau su dung de quy

--- DuyetDoThi/Program.cs
+++ DuyetDoThi/Program.cs
@@ -1,16 +1,62 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DuyetDoThi
+﻿namespace DuyetDoThi
 {
     class Program
     {
-
+        public const int max = 9;
+        public static int[,] m = new int[,]
+        {
+            {0, 1, 1, 0, 0, 0, 0, 0, 0},
+            {1, 0, 1, 0, 0, 0, 0, 0, 1},
+            {1, 1, 0, 1, 1, 0, 0, 0, 0},
+            {0, 0, 1, 0, 1, 0, 0, 0, 1},
+            {0, 0, 1, 1, 0, 1, 1, 0, 0},
+            {0, 0, 0, 0, 1, 0, 0, 0, 0},
+            {0, 0, 0, 0, 1, 0, 0, 1, 0},
+            {0, 0, 0, 0, 0, 0, 1, 0, 0},
+            {0, 1, 0, 1, 0, 0, 0, 0, 0},
+        };
+        public static bool[] chuaxet = new bool[max];
+        public static bool[,] ke = new bool[max, max];
+        // duyet do thi theo chieu sau su dung de quy
+        public static int z = 0;
+        public static void DFSdequy(int v)
+        {
+            //duyet dinh v
+            chuaxet[v] = false;
+            for (int u = 0; u < max; u++)
+            {
+                if (ke[u, v] == true && chuaxet[u] == true)
+                {
+                    DFSdequy(u); z++;
+                }
+            }
+        }
         static void Main(string[] args)
         {
+            // tao mang ke
+            for (int i = 0; i < max; i++)
+            {
+                for (int j = 0; j < max; j++)
+                {
+                    if(m[i,j] == 1)
+                    {
+                        ke[i, j] = true;
+                    }
+                }
+            }
+            // tao nhan cho dinh
+            for (int i = 0; i < max; i++)
+            {
+                chuaxet[i] = true;
+            }
+            // duyet do thi theo chieu sau su dung de quy (co huong va vo huong deu dc)
+            for (int i = 0; i < max; i++)
+            {
+                if (chuaxet[i])
+                {
+                    DFSdequy(i); 
+                }
+            }
         }
     }
 }
